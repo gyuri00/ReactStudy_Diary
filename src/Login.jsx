2b0f8a8656f4bd9d7bfd0b82559b8885a05c4f77
namgyuri <skarbfl0802@gmail.com>
2024-09-19 16:40:11 +0900

    회원가입 화면 수정

---
+++ src/Login.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="page">
+        <div className='titleWrap'>
+            회원가입
+        </div>
+
+        <div className='contentWrap'>
+            <div className='inputTitle'>이메일 주소</div>
+            <div className='inputWrap'>
+                <input 
+                    className='input' placeholder='test@gmail.com'/>
+            </div>
+            <div className='errorMessageWrap'>
+                올바른 이메일을 입력해주세요.
+            </div>
+
+            <div style={{marginTop: "26px"}} className='inputTitle'>비밀번호</div>
+            <div className='inputWrap'>
+                <input 
+                    className='input'
+                    placeholder='영문, 숫자, 특수문자 포함 8자 이상'/>
+            </div>
+            <div className='errorMessageWrap'>
+                영문, 숫자, 특수문자 포함 8자 이상 입력해주세요
+            </div>
+        </div>
+
+        <div>
+            <button disabled={true} className='bottomButton'>
+                확인
+            </button>
+        </div>
+    </div>
+  )
+}
